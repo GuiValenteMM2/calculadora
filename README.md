660a34e5f7db09cf664bb79cf2f201d8d26e61b7
GuiValenteMM2 <guimartins.m.v@gmail.com>
2023-06-06 10:58:41 -0300

    Esqueleto do html, funções básicas js

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # calculadora
 App de calculadora simples. Usando html, css e javascript.
+
+1- Função do display : Quando apertar algum botão, seu valor será guardado,
+logo depois o operador escolhido, e após o segundo número, apertar em =.
+
+display {
+    let firstNumberChosen = document.querySelector(".numberButton");
+    firstNumberChosen.addEventListener('click', storeNumber());
+};
+
---
+++ script.js
@@ -0,0 +1,38 @@
+let firstNumber = "number1";
+let secondNumber = "number2";
+let operator = "operator";
+
+const add = function(a, b) {
+    return a + b;
+};
+
+const sub = function(a, b) {
+    return a - b;
+};
+
+const multiply = function(a, b) {
+    return a * b;
+};
+
+const divide = function(a, b) {
+    return a / b;
+};
+
+const operate = function(firstNumber, operation, secondNumber) {
+    let a = firstNumber;
+    let b = secondNumber;
+    if (operation == add()) {
+        return add(a, b);
+    } 
+    if (operation == sub()) {
+        return sub(a, b);
+    }
+    if (operation == multiply()) {
+        return multiply(a, b);
+    }
+    if (operation == divide()) {
+        return divide(a, b);
+    } else {
+        return "ERRO, ESCOLHA UMA OPERAÇÃO!";
+    };
+};
