949277a1735fdf12f608eda346b299bf44e296cf
GuiValenteMM2 <guimartins.m.v@gmail.com>
2023-06-14 14:25:14 -0300

    Função para salvar o valor do botão pressionado.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let firstNumber = "number1";
-let secondNumber = "number2";
-let operator = "operator";
-
 const add = function(a, b) {
     return a + b;
 };
@@ -18,21 +14,13 @@ const divide = function(a, b) {
     return a / b;
 };
 
-const operate = function(firstNumber, operation, secondNumber) {
-    let a = firstNumber;
-    let b = secondNumber;
-    if (operation == add) {
-        return add(a, b);
-    } 
-    if (operation == sub) {
-        return sub(a, b);
-    }
-    if (operation == multiply) {
-        return multiply(a, b);
-    }
-    if (operation == divide) {
-        return divide(a, b);
-    } else {
-        return "ERRO, ESCOLHA UMA OPERAÇÃO!";
-    };
+const numbersValues = {'firstNumber' : '' , 'operator' : '', 'secondNumber' : ''};
+const displayValue = document.querySelector('#display');
+
+const storeValue = function (el) {
+    let pressedKey = el.value;
+    console.log(pressedKey);
 };
+
+
+
